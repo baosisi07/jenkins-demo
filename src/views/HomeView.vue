d17873ab638166ad5019bd5622c69d2c28ea66dd
baosisi <18018021075@163.com>
2022-09-08 19:18:26 +0800

    config router&UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from "vue";
+const active = ref(0);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <van-nav-bar title="标题" />
+    <van-tabs v-model:active="active" swipeable>
+      <van-tab v-for="index in 4" :title="'选项 ' + index">
+        内容 {{ index }}
+      </van-tab>
+    </van-tabs>
   </main>
 </template>
