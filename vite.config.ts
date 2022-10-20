2ccc3ed500bcc5704092a4fa8ccf376fad3e5b5c
zhanghonglin <289397863@qq.com>
2022-10-20 21:08:06 +0800

    修改部署文件

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,20 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
+    port: 9901,
+    proxy: {
+      '/api_base': {
+        //target: 'http://218.23.56.220:9902/YunWeiNew/app',
+	  target: 'http://localhost:9902/YunWeiNew/app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api_base/, ''),
+      },
+      '/img_pre': {
+        //target: 'http://218.23.56.220:9902/data',
+	  target: 'http://localhost:9902/data',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/img_pre/, ''),
+      },
+    },
   },
 });
